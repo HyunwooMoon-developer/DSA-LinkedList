ceccba2e2192681aadf3edd00f1908180de39c37
Hyunwoo Moon <mhw9163@gmail.com>
2021-02-11 21:46:26 -0800

    working

--- LinkedList.js
+++ LinkedList.js
@@ -41,54 +41,54 @@ class LinkedList {
   }
 
   //insertBefore : inserts a new node before a given node containing a key.
-  insertBefore(newItem, value){
-      if(this.head === null){
-          this.insertFirst(newItem)
-      }
+  insertBefore(newItem, value) {
+    if (this.head === null) {
+      this.insertFirst(newItem);
+    }
 
-      let currentNode = this.head;
-      let previousNode = this.head;
+    let currentNode = this.head;
+    let previousNode = this.head;
 
-      while((currentNode !== null) && (currentNode.value !== value)){
-          previousNode = currentNode;
-          currentNode = currentNode.next;
-      }
+    while (currentNode !== null && currentNode.value !== value) {
+      previousNode = currentNode;
+      currentNode = currentNode.next;
+    }
 
-      const newNode = new _Node(newItem, previousNode.next)
+    const newNode = new _Node(newItem, previousNode.next);
 
-      previousNode.next = newNode;
+    previousNode.next = newNode;
   }
   // insertAfter : inserts a new node after a node containing the key.
-  insertAfter(newItem, value){
-      let currentNode = this.head;
+  insertAfter(newItem, value) {
+    let currentNode = this.head;
 
-      while((currentNode !== null) && (currentNode.value !==value)){
-          currentNode = currentNode.next;
-      }
-      if(value.next === null){
-          this.insertLast(newItem);
-          return ;
-      }
+    while (currentNode !== null && currentNode.value !== value) {
+      currentNode = currentNode.next;
+    }
+    if (value.next === null) {
+      this.insertLast(newItem);
+      return;
+    }
 
-      const newNode = new _Node(newItem, currentNode.next);
-      currentNode.next = newNode;
+    const newNode = new _Node(newItem, currentNode.next);
+    currentNode.next = newNode;
   }
   // insertAt : inserts an item at a specific position in the linked list.
-  insertAt(item, position){
-      if(this.head === null){
-          this.insertFirst(item);
-      }
+  insertAt(item, position) {
+    if (this.head === null) {
+      this.insertFirst(item);
+    }
 
-      let currentNode = this.head;
-      let currentPosition = 0;
-      while(currentPosition  !== position){
-        currentPosition++;
-        currentNode  = currentNode.next;
-      }
+    let currentNode = this.head;
+    let currentPosition = 0;
+    while (currentPosition !== position) {
+      currentPosition++;
+      currentNode = currentNode.next;
+    }
 
-      const newNode = new _Node(item, currentNode.next);
+    const newNode = new _Node(item, currentNode.next);
 
-      currentNode.next = newNode;
+    currentNode.next = newNode;
   }
 
   //get
@@ -133,7 +133,7 @@ class LinkedList {
     //keep track of previous
     let previousNode = this.head;
 
-    while ((currNode !== null) && (currNode.value !== item)) {
+    while (currNode !== null && currNode.value !== item) {
       //save the previous node
       previousNode = currNode;
       currNode = currNode.next;
@@ -149,22 +149,195 @@ class LinkedList {
 const main = function () {
   let SLL = new LinkedList();
 
-  SLL.insertFirst('Apollo');
-  SLL.insertLast('Boomer');
-  SLL.insertLast('Helo');
-  SLL.insertLast('Husker');
-  SLL.insertLast('Starbuck');
+  SLL.insertFirst("Apollo");
+  SLL.insertLast("Boomer");
+  SLL.insertLast("Helo");
+  SLL.insertLast("Husker");
+  SLL.insertLast("Starbuck");
 
-  SLL.insertLast('Tauhida');
+  SLL.insertLast("Tauhida");
 
-  SLL.remove('squirrel');
+  SLL.remove("squirrel");
 
-  SLL.insertBefore('Athena' , 'Boomer');
-  SLL.insertAfter('Hotdog', 'Helo');
-  SLL.insertAt('Kat', 2);
-  SLL.remove('Tauhida');
+  SLL.insertBefore("Athena", "Boomer");
+  SLL.insertAfter("Hotdog", "Helo");
+  SLL.insertAt("Kat", 2);
+  SLL.remove("Tauhida");
+  display(SLL);
+  size(SLL);
+  isEmpty(SLL);
+  findPrevious(SLL, "Kat");
+  findLast(SLL);
+  //reverse(SLL)
+  display(SLL);
+  thirdFromTheEnd(SLL)
+  middleOfList(SLL);
 
-    return SLL;
+  let newSLL = new LinkedList();
+  newSLL.insertFirst(1);
+  newSLL.insertLast(2);
+  newSLL.insertLast(3);
+  newSLL.insertLast(4);
+
+  cycleInList(newSLL);
+  display(newSLL);
 };
+
+// Q3 Supplemental functions for a linked list
+// display: displays the linked list
+// size: returns the size of the linked list
+// isEmpty: finds if the list is empty or not (without using the size() function)
+// findPrevious: finds the node before the item you are looking for
+// findLast: returns the last node in the linked list
+
+//display
+const display = function (list) {
+  let currentNode = list.head;
+  while (currentNode !== null) {
+    console.log(currentNode.value);
+    currentNode = currentNode.next;
+  }
+};
+
+//size
+const size = function (list) {
+  let currentNode = list.head;
+  let size = 0;
+  while (currentNode !== null) {
+    currentNode = currentNode.next;
+    size++;
+  }
+  console.log(size);
+};
+
+//isEmpty
+const isEmpty = function (list) {
+  let currentNode = list.head;
+  if (currentNode === null) {
+    console.log(true);
+    return;
+  }
+  console.log(false);
+  return;
+};
+
+//findPrevious
+/*const findPrevious = function(list, item){
+    let currentNode = list.head;
+    console.log(currentNode);
+    if(currentNode === null){
+      console.log("Item not found");
+      return;
+    }
+    let previousNode = currentNode;
+    while(currentNode !== item){
+      previousNode = currentNode;
+    }
+    return previousNode;
+  }*/
+
+const findPrevious = function (list, item) {
+  let currentNode = list.head;
+  let previousNode = list.head;
+  //console.log(currentNode);
+
+  if (currentNode === null) {
+    console.log("Item not found");
+    return;
+  }
+
+  while (currentNode !== null && currentNode.value !== item) {
+    previousNode = currentNode;
+    currentNode = currentNode.next;
+  }
+  console.log(previousNode.value);
+};
+
+//findLast
+const findLast = function (list) {
+  let currentNode = list.head;
+
+  while (currentNode.next !== null) {
+    currentNode = currentNode.next;
+  }
+  console.log(currentNode.value);
+};
+
+// Q5 Reverse a list
+
+//all pointers should point backward. program should reverse the direction of a given singly linked list.
+function reverse(list) {
+  let currentNode = list.head;
+  let previousNode = null;
+
+  while (currentNode) {
+    let nextNode = currentNode.next;
+    //reverse pointer
+    currentNode.next = previousNode;
+    //increase previous to current
+    previousNode = currentNode;
+    //increase current to next or end of the list
+    currentNode = nextNode;
+  }
+  list.head = previousNode;
+  //change the head;
+  return previousNode; 
+}
+
+// Q6. 3rd from the end
+//Write an algorithm to find the 3rd element from the end of a linked list
+
+function thirdFromTheEnd(list){
+  let currentNode = list.head;
+  if(currentNode === null){
+    console.log("Item not found");
+      return;
+  }
+  while(currentNode.next.next.next !== null){
+    currentNode = currentNode.next;
+  }
+  console.log(currentNode);
+  return;
+}
+
+// Q7 Middle of a list
+//Write an algorithm to find the middle element of a linked list. 
+//finding the size of the linked list using the size() function and dividing it by half will not find the correct middle of the linked list.
+
+
+function middleOfList(list){
+  let currentNode = list.head;
+
+  if(currentNode === null){
+    return null;
+  }
+  else if(currentNode.next === null){
+    return currentNode;
+  }
+  else{
+    let fast = list.head;
+    let slow = list.head;
+    while((fast !== null) && (fast.next !== null)){
+      fast = fast.next.next;
+      slow = slow.next;
+    }
+    console.log(slow);
+    return ;
+  }
+}
+
+// Q8. Cycle in a list
+//Write an algorithm to find whether a linked list has a cycle (i.e., whether a node in the list has its next value pointing to an earlier node in the list).
+
+function cycleInList(list){
+  let currentNode = list.head;
+
+  while(currentNode.next !== null){
+    currentNode = currentNode.next;
+  }
+  currentNode.next = list.head;
+  console.log(list);
+  return;
+}
 
 console.log(main());
